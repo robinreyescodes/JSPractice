f527eedf47a7b429543dc296e1f2eec36e4c69b8
Robin Reyes <robinreyes.code@gmail.com>
2022-05-04 09:02:27 -0700

    js warm up

--- practice.js
+++ practice.js
@@ -1,16 +1,30 @@
-const textarea = document.querySelector("#ourtext");
-const button = document.querySelector("#button");
+const names = ["robin", "renee", "raf", "mom", "dad"];
 
-button.addEventListener("click", (e) => {
-  const array = textarea.value.split("\n");
+//basic js array, slice
+const children = names.slice(0, 3);
 
-  for (let word of array) {
-    word = word.trim();
-    let firstWord = word.slice(0, word.indexOf("_"));
-    let secondWord = word.slice(word.indexOf("_") + 1).toLowerCase();
-
-    let secondUpper =
-      secondWord.slice(0, 1).toUpperCase() + secondWord.slice(1);
-    console.log(firstWord + secondUpper);
+//practicing spread operator and for of loop
+function kids(...names) {
+  for (const name of names) {
+    console.log(name);
   }
-});
+}
+
+kids(...children); //spread operator 'spreads' all the contents of the array
+
+//here is an example of destructuring and rest operator
+//spread opens up, rest gathers the rest and puts them together
+const [oldest, middle, youngest, ...parents] = names;
+
+console.log(`${oldest} is the oldest`);
+console.log(`${middle} is the middle`);
+console.log(`${youngest} is the youngest`);
+
+let string = "";
+//random logic thing
+for (const parent of parents) {
+  if (string == "") {
+    string += `${parent}`;
+  } else string += ` and ${parent} are the parents`;
+}
+console.log(string);
