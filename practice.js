23a01537c5f1d52bb1a6d496660cbfa4f86854ff
Robin Reyes <robinreyes.code@gmail.com>
2022-05-13 09:18:46 -0700

    js challenge 1/warm up

--- practice.js
+++ practice.js
@@ -1,24 +1,20 @@
-//practice problem; return a function from a function
-//example of higher order function
-function redundant(something) {
-  return redundant.bind(something);
-}
-
-const f1 = redundant("apple");
-console.log(f1);
-
-//practice problem #2
-
+//javascript practice problem #1
+// Write a function that takes a string (a) and a number (n) as argument
+// Return the nth character of 'a'
 /* 
-  "Perimeters with a catch" 
-  - without using branch pieces of code, find perimeter of circle or squre 
-  - if 's' is given, perimeter of square
-  - if 'l' is given, perimeter of circle 
-  - only arithmetic/comparison operators 
+  TEST CASES
+  1) myFunction('abcd',1) -> "a"
+  2) myFunction('zyxbwpl',5) -> "w"
+  3) myFunction('gfedcba',3) -> "e"
 */
 
-function perimeter(shape, num) {
-  return shape == "s" ? num * 4 : shape == "c" ? num * 6.28 : null;
+function myFunction(a, n) {
+  //strange that it wants the position of the letter in relation to the string
+  //not like normal indexes...anyway
+
+  return a.charAt(n - 1); //string method finds the char at a certain index
 }
 
-console.log(perimeter("c", 7));
+console.log(myFunction("abcd", 1));
+console.log(myFunction("zyxbwpl", 5));
+console.log(myFunction("gfedcba", 3));
