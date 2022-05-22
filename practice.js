1c159f7343e5d7a2fcdfcfe79cc63ecb5e9bed7b
Robin Reyes <robinreyes.code@gmail.com>
2022-05-22 11:16:51 -0700

    practice problem

--- practice.js
+++ practice.js
@@ -1,81 +1,27 @@
-//javascript practice problem #1
-// Write a function that takes a string (a) and a number (n) as argument
-// Return the nth character of 'a'
-/* 
-  TEST CASES
-  1) myFunction('abcd',1) -> "a"
-  2) myFunction('zyxbwpl',5) -> "w"
-  3) myFunction('gfedcba',3) -> "e"
-*/
+//Practice Problem #1 [Arrays] easy
+const competitions = [
+  ["HTML", "C#"],
+  ["C#", "Python"],
+  ["Python", "HTML"],
+];
 
-function myFunction(a, n) {
-  //strange that it wants the position of the letter in relation to the string
-  //not like normal indexes...anyway
+const results = [0, 0, 1];
 
-  return a.charAt(n - 1); //string method finds the char at a certain index
-}
-
-console.log(myFunction("abcd", 1));
-console.log(myFunction("zyxbwpl", 5));
-console.log(myFunction("gfedcba", 3));
-
-//javascript practice #2
-// Write a function that takes two numbers (a and b) as argument
-// Return b percent of a
-/* 
-  Test Cases
-  1) percentOf(100, 50) -> 50
-  2) percentOf(10, 1) -> 0.1
-  3) percentOf(500, 25) -> 125
-*/
-
-function percentOf(a, b) {
-  //percentage of a number is done by multiplying it by number in such format 0.1, 0.2 etc
-  //take b and divide by 100 to turn it into a decimal
-  //multiply a by that number
-
-  return a * (b / 100);
-}
-
-console.log(percentOf(100, 50));
-console.log(percentOf(10, 1));
-console.log(percentOf(500, 25));
-
-//javascript practice problem #3
-// Write a function that takes two values, say a and b, as arguments
-// Return true if the two values are equal and of the same type
-function myFunction2(a, b) {
-  return a === b;
-}
-
-//== vs === ! all depends on what you need code wise
-
-console.log(myFunction2(2, 3)); //false because not equal in value, but equal in type
-console.log(myFunction2(3, 3)); //true; equal in value and type
-console.log(myFunction2(1, "1")); // false, equal in value but not type
-
-// callback functions
-// tip calculator
-
-const bill1 = billCalculator(30, addTip); //high order function
-const bill2 = billCalculator(60, addTip); //high order function
-
-console.log(`bill #1: ${bill1}`); //template literal
-console.log(`bill #2: ${bill2}`);
-
-/* 
-  our callback function takes the bill and finds the tip amount
-*/
-function addTip(bill) {
-  if (bill < 50) {
-    return bill * 0.2;
+let scores = {};
+let counter = 0;
+for (const game of competitions) {
+  if (results[counter] == 0) {
+    if (!scores[game[1]]) scores[game[1]] = 3;
+    else scores[game[1]] += 3;
   }
-  return bill * 0.4;
+
+  if (results[counter] == 1) {
+    if (!scores[game[0]]) scores[game[0]] = 3;
+    else scores[game[0]] += 3;
+  }
+  counter++;
 }
 
-/* 
-  function that adds up our bill plus the tip from our callback 
-*/
-function billCalculator(bill, callback) {
-  return bill + callback(bill);
+for (const item of Object.entries(scores)) {
+  console.log(Object.values(item));
 }
