e260d101e0eae342d5f7db53b2219dd0af1af041
Robin Reyes <robinreyes.code@gmail.com>
2022-05-31 11:53:23 -0700

    practice warm ups

--- practice.js
+++ practice.js
@@ -1,27 +1,27 @@
-//Practice Problem #1 [Arrays] easy
-const competitions = [
-  ["HTML", "C#"],
-  ["C#", "Python"],
-  ["Python", "HTML"],
-];
-
-const results = [0, 0, 1];
-
-let scores = {};
-let counter = 0;
-for (const game of competitions) {
-  if (results[counter] == 0) {
-    if (!scores[game[1]]) scores[game[1]] = 3;
-    else scores[game[1]] += 3;
-  }
-
-  if (results[counter] == 1) {
-    if (!scores[game[0]]) scores[game[0]] = 3;
-    else scores[game[0]] += 3;
-  }
-  counter++;
+//print all even numbers from 0 - 10
+console.log("practice problem 1");
+for (let i = 2; i <= 10; i += 2) {
+  console.log(i);
 }
 
-for (const item of Object.entries(scores)) {
-  console.log(Object.values(item));
+console.log();
+//print a table containing multiples from 1 - 10
+//let our number be 5
+console.log("practice problem 2");
+let num = 5;
+for (let i = 1; i <= 10; i++) {
+  console.log(num * i);
 }
+
+console.log("\npractice problem 3");
+//turn kilometers to miles with a function
+function toMiles(kilometers) {
+  const miles = kilometers * 0.621371;
+  return `${miles} miles`;
+}
+console.log(toMiles(10));
+
+//get rid of the spaces in a string
+console.log("\npractice problem 4");
+const string = "robin reyes is the coolest";
+console.log(string.replaceAll(" ", ""));
